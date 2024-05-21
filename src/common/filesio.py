0e9bd3e55bb616d6a6c354843c3c859061a8e847
FantasySilence <1735793009@qq.com>
2024-05-22 03:44:36 +0800

    添加了一个文件IO类

---
+++ src/common/filesio.py
@@ -0,0 +1,25 @@
+# ================================================ #
+# @Author: Fantasy_Silence                         #
+# @Time: 2024-05-22                                #
+# @IDE: Visual Studio Code & PyCharm               #
+# @Python: 3.9.7                                   #
+# ================================================ #
+# @Description: 一个文件IO流类                      #
+# ================================================ #
+import os
+
+
+class FilesIO:
+
+    """
+    实现一个文件IO流类用于文件管理
+    """
+
+    @staticmethod
+    def getConfigSavePath(filename: str) -> str:
+
+        src_path = os.path.dirname(os.path.dirname(__file__))
+        ROOTPATH = os.path.dirname(src_path)
+        resources_path = os.path.join(ROOTPATH, 'resources')
+        config_path = os.path.join(resources_path, filename)
+        return config_path
